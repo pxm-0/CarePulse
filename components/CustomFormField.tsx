61d466c7b08294d5896fe32ee1aaf636ecc8a564
pxm-0 <120517870+pxm-0@users.noreply.github.com>
2024-09-24 03:28:48 +0800

    stuck at inputfile issue

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -17,6 +17,13 @@ import 'react-phone-number-input/style.css'
 import PhoneInput from 'react-phone-number-input'
 import { E164Number } from 'libphonenumber-js/core'
 
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+import { Select, SelectContent, SelectTrigger, SelectValue } from './ui/select'
+import { Textarea } from './ui/textarea'
+import { Checkbox } from './ui/checkbox'
+
 
 interface CustomProps {
     control: Control<any>,
@@ -34,7 +41,7 @@ interface CustomProps {
 }
 
 const RenderField = ({ field, props }: { field: any; props: CustomProps}) => {
-    const {fieldType, iconSrc, iconAlt, placeholder } = props;
+    const {fieldType, iconSrc, iconAlt, placeholder, showTimeSelect, dateFormat, renderSkeleton } = props;
 
     switch (props.fieldType) {
         case FormFieldType.INPUT:
@@ -63,7 +70,7 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps}) => {
             <FormControl>
                 <PhoneInput
                     defaultCountry='US'
-                    placeholder={ placeholder }
+                    placeholder={ props.placeholder }
                     international
                     withCountryCallingCode
                     value={ field.value as E164Number | undefined }
@@ -72,7 +79,81 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps}) => {
 
                 />
             </FormControl>
-            )
+            );
+        case FormFieldType.DATE_PICKER:
+            return (
+                <div className='flex rounded-md border border-dark-500 bg-dark-400'>
+                    <Image 
+                    src="/assets/icons/calendar.svg"
+                    height={24}
+                    width={24}
+                    alt='Calendar'
+                    className='ml-2'
+                    />
+                    <FormControl>
+                        <DatePicker 
+                            selected={field.value} 
+                            onChange={(date) => field.onChange(date)} 
+                            dateFormat={dateFormat ?? 'MM/dd/yyyy'}
+                            showTimeSelect={showTimeSelect ?? false}
+                            timeInputLabel='Time'
+                            wrapperClassName='date-picker'
+                        />
+                    </FormControl>
+                </div>
+            );
+        case FormFieldType.SELECT:
+            return (
+                <FormControl>
+                    <Select 
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}>
+                        <FormControl>
+                            <SelectTrigger className='shad-select-trigger'>
+                                <SelectValue placeholder={placeholder} />
+                            </SelectTrigger>
+                        </FormControl>
+                            <SelectContent className='shad-select-content'>
+                                {props.children}
+                            </SelectContent>  
+                        
+                    </Select>
+                </FormControl>
+            );
+        case FormFieldType.SKELETON:
+            return (
+                renderSkeleton ? renderSkeleton(field) : null
+
+            );
+        case FormFieldType.TEXTAREA:
+            return (
+                <FormControl>
+                    <Textarea
+                        placeholder={placeholder}
+                        { ...field }
+                        className='shad-textarea'
+                        disabled={props.disabled}
+                    />
+                </FormControl>
+            );
+        case FormFieldType.CHECKBOX:
+            return (
+                <FormControl>
+                    <div className='flex items-center gap-4'>
+                        <Checkbox 
+                            id={props.name}
+                            checked={field.value}
+                            onCheckedChange={field.onChange} />
+                        <label htmlFor={props.name} className='checkbox-label'>
+                            {props.label}
+                        </label>
+                    </div>
+                </FormControl>
+            );
+
+        default:
+        break;
+        
     }
 }
 
--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,6 +1,9 @@
-import { ID, Query } from "node-appwrite"
-import { users } from "../appwrite.config"
+"use server"
+
+import { ID, Query, InputFile } from "node-appwrite"
+import { BUCKET_ID, DATABASE_ID, ENDPOINT, PATIENT_COLLECTION_ID, PROJECT_ID, databases, storage, users } from "../appwrite.config"
 import { parseStringify } from "../utils"
+
 
 export const createUser = async (user: CreateUserParams) => {
 
@@ -26,3 +29,41 @@ export const createUser = async (user: CreateUserParams) => {
       console.error("An error occurred while creating a new user:", error);
     }
   };
+
+export const getUser = async (userId: string) => {
+    try {
+        const user = await users.get(userId);
+        parseStringify(user)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const registerPatient = async ( {identificationDocument, ...patient }: RegisterUserParams) => {
+  try {
+    let file;
+    if (identificationDocument) {
+      const inputFile = InputFile.fromBuffer(
+        identificationDocument?.get('blobFile') as Blob,
+        identificationDocument?.get('fileName') as string
+      )
+      file = await storage.createFile(BUCKET_ID, ID.unique(), inputFile, ['*'])
+    }
+
+    const newPatient = await databases.createDocument(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      ID.unique(),
+      {
+        ...patient,
+        identificationDocumentId: file?.$id || null,
+        identificationDocumentUrl: `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file?.$id}/view?project=${PROJECT_ID}`
+      }
+    )
+    return parseStringify(newPatient)
+
+  } catch (error) {
+    console.log(error)
+    
+  }
+}
